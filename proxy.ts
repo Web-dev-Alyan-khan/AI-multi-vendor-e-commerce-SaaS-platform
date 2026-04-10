e87e235e31f8df578eddbfc2981469ae8b456845
Web-Alyan-khan <alyanalyanofficial@gmail.com>
2026-04-10 11:31:48 +0500

    completed projects

--- proxy.ts
+++ proxy.ts
@@ -1,12 +1,22 @@
-import { clerkMiddleware } from '@clerk/nextjs/server';
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
 
-export default clerkMiddleware();
+const isPublicRoute = createRouteMatcher([
+  '/', 
+  '/api/inngest', 
+  '/sign-in(.*)', 
+  '/sign-up(.*)'
+]);
+
+// Next.js 16 expects a direct export for the proxy function
+export default clerkMiddleware(async (auth, request) => {
+  if (!isPublicRoute(request)) {
+    await auth.protect();
+  }
+});
 
 export const config = {
   matcher: [
-    // Skip Next.js internals and all static files, unless found in search params
     '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
-    // Always run for API routes
     '/(api|trpc)(.*)',
   ],
 };
